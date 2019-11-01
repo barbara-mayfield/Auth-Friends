c812c046a84bffae22010050fe1348c14fb50ff5
Barbara Mayfield <bmayfield1991@gmail.com>
2019-11-01 13:03:06 -0500

    Added routing to Nav component, created simple login form in Signin.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+
 import Nav from './components/layout/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <Nav logo='far fa-grin-beam fa-1x' title='Friends App' />
+    <div className='App'>
+        <Nav logo='far fa-grin-beam fa-1x' title='Friends App' />  
     </div>
   );
 }
--- friends/src/components/layout/Nav.js
+++ friends/src/components/layout/Nav.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
+import Signin from './Signin'
 
 export default function Nav(props) {
-    console.log(props, "<- props")
+    console.log(props, '<- Nav props')
     return (
-        <div>
+        <div className='navbar bg-dark'>
             <h1>
                 <i className={props.logo} /> {props.title}
             </h1>
+            
+            <div className='ul'>
+                <Link to='/'>Home</Link>
+                <Link to='/signin'>Sign In</Link>
+            </div>
+
+            <Route path='/signin' component={Signin} />
         </div>
     )
 }
